8d3f5fbc2c138a5d3a541f4c7f136663a73831e0
Mike Kurkin <mike@kurkin.net>
2022-07-17 15:44:38 +0300

    Record model

--- wildchord/editor/admin.py
+++ wildchord/editor/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
+from .models import Record, User
+
+
+class RecordAdmin(admin.ModelAdmin):
+    # Disable default behaviour of stripping contents in admin field
+    def formfield_for_dbfield(self, db_field, request, **kwargs):
+        if db_field.name == 'contents':
+            kwargs['strip'] = False
+        return super().formfield_for_dbfield(db_field, request, **kwargs)
+
+
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Record, RecordAdmin)
--- wildchord/editor/models.py
+++ wildchord/editor/models.py
@@ -1,3 +1,43 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+
 # Create your models here.
+class User(AbstractUser):
+    pass
+
+
+class Record(models.Model):
+    author = models.ForeignKey(
+        "User", on_delete=models.PROTECT, related_name="records")
+    contents = models.TextField(max_length=10000, blank=True, null=True)
+    create_timestamp = models.DateTimeField(auto_now_add=True)
+    update_timestamp = models.DateTimeField(auto_now=True)
+
+    @classmethod
+    def nth_non_empty_line(self, ls, n):
+        if ls is None or n <= 0:
+            return None
+        i = 0
+        while i < n:
+            if ls.strip() == '':
+                return None
+            l, ls = ls.split('\n', 1)
+            if l.strip() != '':
+                i += 1
+        return l
+
+    def save(self):
+        # Add new line to the end of the contents
+        if self.contents == '' or self.contents[-1] != '\n':
+            self.contents += '\n'
+        return super().save()
+
+    def title_line(self):
+        return "New Record" if (first := Record.nth_non_empty_line(self.contents, 1)) is None else first[:100]
+
+    def second_line(self):
+        return "No text" if (line := Record.nth_non_empty_line(self.contents, 2)) is None else line[:100]
+
+    def __str__(self):
+        return f'Record #{self.id}: {self.title_line()}'
